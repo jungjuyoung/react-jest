04c32c16f6265c3a2e0e55e7dabd1ff1c97e597b
jungjuyoung <nwwn1004@naver.com>
2021-11-20 01:47:28 +0900

    feat: mongDB connected my project

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const PORT = 5000;
 const app = express();
-
 const productRoutes = require("./route/app");
+
+const config = require("./config/key");
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`mongDB Connected....`))
+  .catch(err => console.log(err));
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
