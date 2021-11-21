31aaa33ef0aa968ca911d2c9cf56716299961e21
jungjuyoung <nwwn1004@naver.com>
2021-11-22 03:04:56 +0900

    feat: solving async problems with asyn and  await

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -3,20 +3,6 @@ const productModel = require("../../models/Product");
 const httpMocks = require("node-mocks-http");
 const newProduct = require("../data/product.json");
 
-// 무엇을 테스트 할 것인가? prduct를 DB에 CRUD 테스트.
-// C: create product 테스트
-// 1. productControlle에 createProduct함수가 있는지 확인.
-// 2.  productControlle에 createProduct 함수의 기능 unit 테스트
-// unit 테스트를 하며 실제 DB에 영향이 가기면 안돼기 때문에 mock 함수를 작성하여 DB의 데이터를 로컬로 복사해와서 테스트한다.
-
-// mock 함수
-// const mockFunction = jest.fn();
-// mockFunction()
-// mockFunction('hello')
-// console.log(mockFunction) //hello
-// expect(mockFunction).toBeCalledWith('hello')
-// expect(mockFunction).toBelCalledTimes(2)
-
 //mock 함수 생성
 productModel.create = jest.fn();
 
@@ -29,30 +15,29 @@ beforeEach(() => {
 
 describe("Product Controller Create", () => {
   beforeEach(() => {
-    // 매번 시작할때 req.body에 newProduct넣어줌
-    req.body = newProduct;
+    req.body = newProduct; // 매번 실행할 때마다 req.body에 newProduct 넣어줌
   });
 
   it("shold have a createProduct function", () => {
     expect(typeof productController.createProduct).toBe("function");
   });
 
-  it("should call productModel.create", () => {
+  it("should call productModel.create", async () => {
     // productController.createProduct() 호출이 될 때
     // productModel.create 호출이 되는지 (mongDB에 model create해주는 함수)
-    productController.createProduct(req, res, next); // 함수안에 productModel.craete함수를 호출 함.
+    await productController.createProduct(req, res, next); // 함수안에 productModel.craete함수를 호출 함.
     expect(productModel.create).toBeCalledWith(req.body);
   });
 
-  it("should return 201 statement code", () => {
-    productController.createProduct(req, res, next);
+  it("should return 201 statement code", async () => {
+    await productController.createProduct(req, res, next);
     expect(res.statusCode).toBe(201);
     expect(res._isEndCalled()).toBeTruthy();
   });
 
-  it("should return json body in response", () => {
+  it("should return json body in response", async () => {
     productModel.create.mockReturnValue(newProduct);
-    productController.createProduct(req, res, next);
+    await productController.createProduct(req, res, next);
     expect(res._getJSONData()).toStrictEqual(newProduct);
   });
 });
