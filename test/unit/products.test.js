7c400e8f647435226990158b34db45b0305fedd7
jungjuyoung <nwwn1004@naver.com>
2021-11-21 23:02:06 +0900

    feat:passed status value

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,5 +1,8 @@
 const productController = require("../../controller/products");
 const productModel = require("../../models/Product");
+const httpMocks = require("node-mocks-http");
+const newProduct = require("../data/product.json");
+
 // 무엇을 테스트 할 것인가? prduct를 DB에 CRUD 테스트.
 // C: create product 테스트
 // 1. productControlle에 createProduct함수가 있는지 확인.
@@ -17,7 +20,18 @@ const productModel = require("../../models/Product");
 //mock 함수 생성
 productModel.create = jest.fn();
 
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 describe("Product Controller Create", () => {
+  beforeEach(() => {
+    req.body = newProduct;
+  });
+
   it("shold have a createProduct function", () => {
     expect(typeof productController.createProduct).toBe("function");
   });
@@ -25,7 +39,13 @@ describe("Product Controller Create", () => {
   it("should call productModel.create", () => {
     // productController.createProduct() 호출이 될 때
     // productModel.create 호출이 되는지 (mongDB에 model create해주는 함수)
-    productController.createProduct();
-    expect(productModel.create).toBeCalled();
+    productController.createProduct(req, res, next); // 함수안에 productModel.craete함수를 호출 함.
+    expect(productModel.create).toBeCalledWith(req.body);
+  });
+
+  it("should return 201 statement code", () => {
+    productController.createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
   });
 });
