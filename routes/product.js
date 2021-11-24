2f257aaf879d41de21bcf03f17af6d1dba59ecf4
jungjuyoung <nwwn1004@naver.com>
2021-11-25 04:09:58 +0900

    feat:getProductById integration test

--- routes/product.js
+++ routes/product.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controller/products");
 
-// POST /api/products
+// POST / api/products
 router.post("/", productController.createProduct);
 // GET / api/products
 router.get("/", productController.getProduct);
+// GET / api/productByID
+router.get("/:productId", productController.getProductById);
+
 module.exports = router;
--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 const app = require("../../server");
 const newProduct = require("../data/product.json");
 
+let firstProduct;
+
 describe("Product Integration Create", () => {
   it("POST / api / products", async () => {
     const response = await request(app).post("/api/products").send(newProduct);
@@ -32,5 +34,15 @@ describe("Product Integration Read", () => {
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].name).toBeDefined();
     expect(response.body[0].description).toBeDefined();
+    firstProduct = response.body[0];
+  });
+
+  it("GET /api / productId", async () => {
+    const response = await request(app).get(
+      "/api/products/" + firstProduct._id
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.body.name).toBe(firstProduct.name);
+    expect(response.body.description).toBe(firstProduct.description);
   });
 });
